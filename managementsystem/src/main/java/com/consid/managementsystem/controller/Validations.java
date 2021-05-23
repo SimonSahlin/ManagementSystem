a57fabe6ca3b4e710f2b4c91f1bb84037126b3e9
Simon Sahlin <simon.sahlin@utb.ecutbildning.se>
2021-05-23 13:12:16 +0200

    fixed all validations for the program to work

--- managementsystem/src/main/java/com/consid/managementsystem/controller/Validations.java
+++ managementsystem/src/main/java/com/consid/managementsystem/controller/Validations.java
@@ -39,32 +39,6 @@ public class Validations {
         return con;
     }
 
-    //Function to check if there is a CEO already pressent in the db. 
-    public String checkForCeo(@ModelAttribute("employee") Employee employee) throws SQLException{
-        Statement stmt = connectionForQuery().createStatement();
-        ResultSet rs = stmt.executeQuery("select is_ceo from employees");
-        
-        String checkForCeo = "false";
-
-        LinkedList<String> employeeList = new LinkedList<>();
-        //Adds all the "is_ceo" values into a linkedlist.
-        while(rs.next()){
-            String addCeo = rs.getString("is_ceo");
-            employeeList.add(addCeo);
-        }
-        connectionForQuery().close();
-        //If-statement that checks if the above linkedlist does include a CEO
-        if(employee.getIsCeo().equals("Yes")){
-            for(int i = 0; i < employeeList.size(); i++){
-                if(employeeList.get(i).equals("Yes")){
-                    checkForCeo = "true";
-                    return checkForCeo;
-                }
-            } 
-        }
-        return checkForCeo;
-    }
-
     //fetch all employeeIds from the database and return a LinkedList with all employeeIds.
     public LinkedList<String> fetchEmployeeId() throws SQLException{
         Statement stmt = connectionForQuery().createStatement();
@@ -80,6 +54,7 @@ public class Validations {
         return employeeIds;
 
     }
+    
     //fetch all is_manager from the database and return a LinkedList with all values.
     public LinkedList<String> fetchIsManager() throws SQLException{
         Statement stmt = connectionForQuery().createStatement();
@@ -93,14 +68,71 @@ public class Validations {
         connectionForQuery().close();
         return isManager;
     }
+    
+    //fetch the employee that matches specific employeeId
+    public String fetchEmployeeById(long id) throws SQLException{
+        Statement stmt = connectionForQuery().createStatement();
+        ResultSet rs = stmt.executeQuery("select * from employees WHERE id = " + id);
 
+        String employeeManager = "";
+
+        while(rs.next()){
+            employeeManager = rs.getString("is_manager");
+        }
+        connectionForQuery().close();
+        return employeeManager;
+    }
+
+    //Fetch all managerIds
+    public LinkedList<Integer> fetchAllManagerIds() throws SQLException{
+        Statement stmt = connectionForQuery().createStatement();
+        ResultSet rs = stmt.executeQuery("select manager_id from employees");
+
+        LinkedList<Integer> allEmployeeIds = new LinkedList<>();
+ 
+        while(rs.next()){
+            int thisEmployeeId = rs.getInt("manager_id");
+            allEmployeeIds.add(thisEmployeeId);
+        }
+        connectionForQuery().close();
+        return allEmployeeIds;
+    }
+    
+    //Function to check if there is a CEO already pressent in the db. 
+    public String checkForCeo(@ModelAttribute("employee") Employee employee) throws SQLException{
+        Statement stmt = connectionForQuery().createStatement();
+        ResultSet rs = stmt.executeQuery("select is_ceo from employees");
+        
+        String checkForCeo = "";
+
+        LinkedList<String> employeeList = new LinkedList<>();
+        //Adds all the "is_ceo" values into a linkedlist.
+        while(rs.next()){
+            String addCeo = rs.getString("is_ceo");
+            employeeList.add(addCeo);
+        }
+        connectionForQuery().close();
+        //If-statement that checks if the above linkedlist does include a CEO
+        if(employee.getIsCeo().equals("Yes")){
+            for(int i = 0; i < employeeList.size(); i++){
+                if(employeeList.get(i).equals("Yes")){
+                    checkForCeo = "true";
+                    return checkForCeo;
+                } else {
+                    checkForCeo = "false";
+                }
+            } 
+        }
+        return checkForCeo;
+    }
+    
     //Checking if the employeeId is valid and if the EmployeeId is a manager.
     public String checkForEmployeeIdAsManagerId(@ModelAttribute("employee") Employee employee) throws SQLException{
         
         LinkedList<String> employeeIds = fetchEmployeeId();
         LinkedList<String> isManager = fetchIsManager();
 
-        String employeeIdForManager = "false";
+        String employeeIdForManager = "";
 
         if(employeeIds.size()==0){
             return employeeIdForManager = "true";
@@ -110,10 +142,39 @@ public class Validations {
             if(employee.getManagerId().equals(employeeIds.get(i)) && isManager.get(i).equals("Yes")){
                 //Check if the EmployeeId is a manager.
                 employeeIdForManager = "true";
+                employeeIds.clear();
+                isManager.clear();
                 return employeeIdForManager;
             }
         } 
+        employeeIdForManager = "false";
         return employeeIdForManager;
+    }
+
+    //Checking if the deleted employee is a manager over another employee.
+    public String isTheEmployeeAManager(long id) throws SQLException{
+        String isTheEmployeeAManager = fetchEmployeeById(id);
+
+        if (isTheEmployeeAManager.equals("Yes")){
+            //Would return a Yes and the manager cant be removed until the other employees are updated.
+            return isTheEmployeeAManager;
+        }
+        return isTheEmployeeAManager;
+    }
+
+    public String doesTheManagerManageAnyone(long id) throws SQLException{
+        LinkedList<Integer> allManagerIds = fetchAllManagerIds();
+
+        String theManagerManageAnEmployee = "No";
+
+        for (int i = 0; i  < allManagerIds.size(); i++){
+            if(allManagerIds.get(i) == id){
+                theManagerManageAnEmployee = "Yes";
+                allManagerIds.clear();
+                return theManagerManageAnEmployee;
+            }
+        }
+        return theManagerManageAnEmployee;
     }
 
 }
