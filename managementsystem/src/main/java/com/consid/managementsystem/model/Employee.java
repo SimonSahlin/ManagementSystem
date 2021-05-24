3cd8f42df26df7d1304d43409e81b88546207df3
Simon Sahlin <simon.sahlin@utb.ecutbildning.se>
2021-05-24 08:44:09 +0200

    smaller fixes, all works

--- managementsystem/src/main/java/com/consid/managementsystem/model/Employee.java
+++ managementsystem/src/main/java/com/consid/managementsystem/model/Employee.java
@@ -12,7 +12,6 @@ import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.Range;
 
-
 @Entity
 @Table(name = "employees")
 public class Employee {
@@ -22,59 +21,69 @@ public class Employee {
 
     @Column(name = "name")
     @NotNull()
-    @Size(min = 2, max = 60, message="Name has to be atleast 2 characters and no more than 60 characters.")
+    @Size(min = 2, max = 60, message = "Name has to be atleast 2 characters and no more than 60 characters.")
     private String name;
-    
+
     @Column(name = "salary")
     @NotNull()
     @Range(min = 1, message = "You need to choose a rank!")
     private int salary;
 
     @Column(name = "is_manager")
-    @NotEmpty(message ="Choose Yes or No")
+    @NotEmpty(message = "Choose Yes or No")
     private String ismanager;
 
     @Column(name = "is_ceo")
-    @NotEmpty(message ="Choose Yes or No")
+    @NotEmpty(message = "Choose Yes or No")
     private String isceo;
 
     @Column(name = "manager_id")
     private String managerid;
-    
-    
+
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getIsManager() {
         return ismanager;
     }
+
     public void setIsManager(String ismanager) {
         this.ismanager = ismanager;
     }
+
     public String getIsCeo() {
         return isceo;
     }
+
     public void setIsCeo(String isceo) {
         this.isceo = isceo;
     }
+
     public int getSalary() {
         return salary;
     }
+
     public void setSalary(int salary) {
         this.salary = salary;
     }
+
     public String getManagerId() {
         return managerid;
     }
+
     public void setManagerId(String managerid) {
         this.managerid = managerid;
     }
--- managementsystem/src/main/java/com/consid/managementsystem/service/EmployeeService.java
+++ managementsystem/src/main/java/com/consid/managementsystem/service/EmployeeService.java
@@ -6,12 +6,20 @@ import com.consid.managementsystem.model.Employee;
 
 public interface EmployeeService {
     List<Employee> getAllEmployees();
+
     void saveEmployee(Employee employee);
+
     Employee getEmployeeById(int id);
+
     void deleteEmployeeById(int id);
+
     public int calculateSalary(Employee employee);
+
     public String checkForCeo(Employee employee);
+
     public String checkForEmployeeIdAsManagerId(Employee employee);
+
     public String isTheEmployeeAManager(int id);
+
     public String doesTheManagerManageAnyone(int id);
 }
